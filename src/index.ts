dad1814a3e021f6d487e659dbe82db0442993715
Guilherme <guilherme@intruder.network>
2021-06-16 13:56:18 -0400

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import express from "express";
+import { json } from "body-parser";
+import cors from "cors";
+
+const app = express();
+
+app.use(json());
+app.use(cors());
+
+const PORT = 8899;
+const ADDRESS = "0.0.0.0";
+
+app.get("/", (req, res) => {
+  const body = req.query.body as string;
+
+  if (!body) {
+    res.sendStatus(200);
+    return;
+  }
+
+  try {
+    const decoded = JSON.parse(Buffer.from(body, "base64").toString("ascii"));
+    res.send(decoded);
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+app.listen(PORT, ADDRESS, () => {
+  console.log(`Listening on: ${PORT}${ADDRESS}...`);
+});
