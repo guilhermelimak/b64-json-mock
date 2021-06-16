18b95f67232aa57c2f1d5e2377b274876ee5064c
Guilherme <guilherme@intruder.network>
2021-06-16 14:50:02 -0400

    feat: Add logging

--- functions/index.ts
+++ functions/index.ts
@@ -2,20 +2,25 @@ import { Handler } from "@netlify/functions";
 
 const handler: Handler = (event, context) => {
   const body = event.queryStringParameters?.body;
+  console.log("Receiving body", body);
 
   if (!body) {
+    console.log("No body, returning 500. Body received: ", body);
     return {
       statusCode: 500,
     };
   }
 
   try {
+    console.log("Decoding buffer");
     const decoded = Buffer.from(body, "base64").toString("ascii");
+    console.log("Buffer decoded:", decoded);
     return {
       statusCode: 200,
       body: decoded,
     };
   } catch (err) {
+    console.log("Fail to decode buffer");
     console.error(err);
     return {
       statusCode: 500,
